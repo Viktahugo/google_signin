69af58bc79be8ed15e74e2bc7b9077482df7dc94
viktahugo91@gmail.com <viktahugo91@gmail.com>
2022-06-21 11:05:25 +0200

    First Commit

---
+++ gconfig.php
@@ -0,0 +1,26 @@
+<?php
+
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId('');# Enter Your Google Client Id
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret(''); # Enter Your Client Secret Key
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri(''); #Enter Your Redirect Url
+
+//
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+//start session on web page
+session_start();
+
+?>
